e4f72558e63d7b482eecf28dfb54c9eb495cc522
Aulia Ramadhan <auliarama965@gmail.com>
2023-11-02 11:23:33 +0700

    [Update] Fetch API

---
+++ src/components/Popular.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style/Home.css'
+
+function Popular(props) {
+    const {title, image} = props
+  return (
+    <div className="menu-child">
+            <p>{title}</p>
+            <img src={image} />
+    </div>
+  )
+}
+
+export default Popular
